e1746113578534745cee2629b51c6c0ffa02f874
Chester Swann-Auger <chester@hey.com>
2024-09-15 18:01:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $players = loadScores($games);
 
 ?>
 <head>
-    <title>Techquity Pool League</title>
+    <title>Techquity Pool Leaguea</title>
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
     <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
     <link rel="stylesheet" href="style.css">

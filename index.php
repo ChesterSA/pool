f7dc0f68257eeb81e85709bcb9a2146b39814f15
Chester Swann-Auger <chester@hey.com>
2024-10-11 11:18:24 +0100

    fix

--- index.php
+++ index.php
@@ -6,14 +6,18 @@ $games = loadGames();
 $players = loadScores($games);
 
 ?>
+<!doctype html>
+<html>
 <head>
     <title>Techquity Pool League</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <meta charset="utf-8">
+
     <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
-    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
 <header>
+
     <h1>Techquity Pool League</h1>
     <nav>
         <a href="new-player.php">Add a Player</a>
@@ -21,57 +25,58 @@ $players = loadScores($games);
     </nav>
 </header>
 
-<div class="container">
-    <div class="container">
-        <div style="display: flex; justify-content: center;">
+<main>
+    <div style="display: flex; justify-content: center;">
 
-            <table>
-                <tr>
-                    <th>Player</th>
-                    <th>Wins</th>
-                    <th>Losses</th>
-                    <th>Ratio</th>
-                    <th>Score</th>
-                </tr>
+        <table>
+            <tr>
+                <th>Player</th>
+                <th>Wins</th>
+                <th>Losses</th>
+                <th>Ratio</th>
+                <th>Score</th>
+            </tr>
 
-                <?php
-                $loop = 1;
+            <?php
+            $loop = 1;
 
-                foreach ($players as $player => $stats) {
-                    $key = match ($loop) {
-                        1 => '&#127942;',
-                        8 => '&#127921;',
-                        default => "$loop."
-                    };
-                    echo "<tr><td>$key $player</td><td>{$stats['wins']}</td><td>{$stats['losses']}</td><td>{$stats['ratio']}</td><td>{$stats['elo']}</td></tr>";
-                    $loop++;
-                }
-                ?>
-            </table>
+            foreach ($players as $player => $stats) {
+                $key = match ($loop) {
+                    1 => '&#127942;',
+                    8 => '&#127921;',
+                    default => "$loop."
+                };
+                echo "<tr><td>$key $player</td><td>{$stats['wins']}</td><td>{$stats['losses']}</td><td>{$stats['ratio']}</td><td>{$stats['elo']}</td></tr>";
+                $loop++;
+            }
+            ?>
+        </table>
 
-        </div>
-        <details>
-            <summary>Games</summary>
-            <table>
-                <tr>
-                    <th>Winner</th>
-                    <th>Loser</th>
-                    <th>Date</th>
-                </tr>
-
-                <?php
-                foreach ($games as $game) {
-                    echo "<tr>
-                            <td>{$game['winner']}</td>
-                            <td>{$game['loser']}</td>
-                            <td>{$game['date']}</td>
-                          </tr>";
-                }
-                ?>
-            </table>
-        </details>
     </div>
+    <details>
+        <summary>Games</summary>
+        <table>
+            <tr>
+                <th>Winner</th>
+                <th>Loser</th>
+                <th>Date</th>
+            </tr>
 
-</div>
+            <?php
+            foreach ($games as $game) {
+                echo "<tr>
+                        <td>{$game['winner']}</td>
+                        <td>{$game['loser']}</td>
+                        <td>{$game['date']}</td>
+                      </tr>";
+            }
+            ?>
+        </table>
+    </details>
+</main>
+<footer>
+    <p>Made with ❤️ and ☕️ by Blake Watson.</p>
+</footer>
 
 </body>
+</html>
